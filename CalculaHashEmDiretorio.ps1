1301db694aeeb8ec058826e4cca52996ebf0a61e
FmsJoao <joao.victorfmaciel@gmail.com>
2022-09-29 17:51:24 -0300

    teste

--- CalculaHashEmDiretorio.ps1
+++ CalculaHashEmDiretorio.ps1
@@ -5,7 +5,7 @@ $ErrorActionPreference = "Stop"
 . .\ShaFile.Ps1
 
 $arquivos = Get-ChildItem $diretorio -File
-
+# a
 foreach ($item in $arquivos.FullName){
     $hashItem = Get-FileSHA1 $item
     Write-Host "O Hash do arquivo $item eh $hashItem ! "
