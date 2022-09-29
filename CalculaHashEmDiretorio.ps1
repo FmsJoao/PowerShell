272b053a71e6dd921ebebf23055c9544b8af668b
FmsJoao <joao.victorfmaciel@gmail.com>
2022-09-29 17:22:30 -0300

    Initial commit

---
+++ CalculaHashEmDiretorio.ps1
@@ -0,0 +1,12 @@
+param($diretorio)
+
+$ErrorActionPreference = "Stop"
+
+. .\ShaFile.Ps1
+
+$arquivos = Get-ChildItem $diretorio -File
+
+foreach ($item in $arquivos.FullName){
+    $hashItem = Get-FileSHA1 $item
+    Write-Host "O Hash do arquivo $item eh $hashItem ! "
+}
