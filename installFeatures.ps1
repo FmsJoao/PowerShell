d65c952929104de4191372ae3907ff2a9c00666e
FmsJoao <joao.victorfmaciel@gmail.com>
2022-09-29 23:55:41 -0300

    creating code to install features

---
+++ installFeatures.ps1
@@ -0,0 +1,18 @@
+function Install-WindowsFeatureInServers {
+    param(
+        [String[]] $computers,
+        [String] $featureName
+    )
+
+    $jobScriptBlock = {
+        param(
+            [String] $computerName,
+            [String] $featureName
+        )
+        Install-WindowsFeature -ComputerName $computerName -Name $featureName
+    }
+    $computers | ForEach-Object {
+        Start-Job -ScriptBlock $jobScriptBlock -ArgumentList($_, $featureName)
+
+    }
+}
